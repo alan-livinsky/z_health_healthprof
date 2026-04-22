a4cbb464e789baad9c114e4c065403418e9c7364
alan5411 <alan.livinsky@gmail.com>
2026-04-22 14:13:01 -0300

    refactor(healthprofessional): remove practitioner status indicator field
    Drop the custom is_healthprof function field implementation from the health professional model.
    Remove the corresponding field entry from the tree view definition.
    Clean up the previously added practitioner flag implementation that is no longer in use.

--- health.py
+++ health.py
@@ -1,16 +1,5 @@
-from trytond.model import fields
 from trytond.pool import PoolMeta
 
 
 class HealthProfessional(metaclass=PoolMeta):
     __name__ = 'gnuhealth.healthprofessional'
-
-    is_healthprof = fields.Function(
-        fields.Char('Es médico?'),
-        'get_is_healthprof'
-    )
-
-    def get_is_healthprof(self, name):
-        if self.name:
-            return 'Sí' if self.name.is_healthprof else 'No'
-        return 'No'
