cd45cbdf02fa54d1dc208cda715871cff5d60af2
alan5411 <alan.livinsky@gmail.com>
2026-04-22 13:25:58 -0300

    refactor(healthprofessional): rename doctor status field identifier
    Standardize field naming by renaming is_doctor_display function field
    to is_doctor_text. Update matching getter method and tree view column
    reference to maintain functionality after rename.

--- health.py
+++ health.py
@@ -5,12 +5,12 @@ from trytond.pool import PoolMeta
 class HealthProfessional(metaclass=PoolMeta):
     __name__ = 'gnuhealth.healthprofessional'
 
-    is_doctor_display = fields.Function(
+    is_doctor_text = fields.Function(
         fields.Char('Es un medico?'),
-        'get_is_doctor_display'
+        'get_is_doctor_text'
     )
 
-    def get_is_doctor_display(self, name):
+    def get_is_doctor_text(self, name):
         if self.name and self.name.is_healthprof:
             return 'si'
         return 'no'
