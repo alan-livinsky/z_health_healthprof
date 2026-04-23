0d914fed28a2c25ecae376e732226f81c6e7e04a
alan5411 <alan.livinsky@gmail.com>
2026-04-23 16:28:19 -0300

    feat(healthprofessional): add is doctor indicator field to health professional tree view
    Add a function field to display whether a health professional is a doctor in the tree view, based on the associated party's health professional status.

--- health.py
+++ health.py
@@ -1,5 +1,14 @@
+from trytond.model import fields
 from trytond.pool import PoolMeta
 
 
 class HealthProfessional(metaclass=PoolMeta):
     __name__ = 'gnuhealth.healthprofessional'
+
+    party_is_healthprof = fields.Function(
+        fields.Boolean('Es médico?'),
+        'get_party_is_healthprof'
+    )
+
+    def get_party_is_healthprof(self, name):
+        return bool(self.name and self.name.is_healthprof)
